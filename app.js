1a0751967bf65c53fedc1412644586cde7d547da
Charles Bergeron <chuck.bergeron@gmail.com>
2013-02-09 14:59:40 -0700

    env vars for twitter

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ var express = require('express')
 config = konphyg.all();
 
 Twitter = new twit({
-    consumer_key:         config.twitter.consumer_key
-  , consumer_secret:      config.twitter.consumer_secret
-  , access_token:         config.twitter.access_token
-  , access_token_secret:  config.twitter.access_token_secret
+  consumer_key:         process.env.MOODY_TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.MOODY_TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.MOODY_TWITTER_ACCESS_TOKEN,
+  access_token_secret:  process.env.MOODY_TWITTER_ACCESS_TOKEN_SECRET
 })
 
 var app = express();
