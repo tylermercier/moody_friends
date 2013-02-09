fc4f7ab962e456a04838c201da9d254d1ebc09d3
Tyler Mercier <tylermercier@gmail.com>
2013-02-09 13:50:19 -0700

    cleanup app.js

--- app.js
+++ app.js
@@ -13,38 +13,6 @@ var express = require('express')
 
 var app = express();
 
-// start bad
-
-// if(process.env.VCAP_SERVICES){
-//   var env = JSON.parse(process.env.VCAP_SERVICES);
-//   var mongo = env['mongodb-1.8'][0]['credentials'];
-// }
-// else{
-//   var mongo = {
-//   "hostname":"localhost",
-//   "port":27017,
-//   "username":"",
-//   "password":"",
-//   "name":"",
-//   "db":"db"
-//   }
-// }
-// var generate_mongo_url = function(obj){
-//   obj.hostname = (obj.hostname || 'localhost');
-//   obj.port = (obj.port || 27017);
-//   obj.db = (obj.db || 'test');
-//   if(obj.username && obj.password){
-//     return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
-//   }
-//   else{
-//     return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
-//   }
-// }
-// var mongourl = generate_mongo_url(mongo);
-
-// end bad
-
-
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -69,7 +37,7 @@ app.get("/test", function (request, response) {
   var SentimentClassifier = require('node-sentiment');
   var classifier = new SentimentClassifier;
 
-  var result = classifier.classify('i hate open source');
+  var result = classifier.classify('nodejs is sorta cool');
   response.send(result);
 });
 
