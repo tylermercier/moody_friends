abaa262d3730f2bacaf6a82698dd0201927c4939
Joshua Pinter <joshuapinter@gmail.com>
2013-02-09 14:22:41 -0700

    Setup konphyg.

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , twit = require('twit')
+  , konphyg = require('konphyg')('./config');
+  
+config = konphyg.all();
 
 var app = express();
 
