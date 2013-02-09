a6844138e7275183046f3b4514c0998233c1f942
Charles Bergeron <chuck.bergeron@gmail.com>
2013-02-09 12:42:26 -0700

    updates to the script, store it in the src dir

--- mongo_install.sh
+++ mongo_install.sh
@@ -1,10 +1,10 @@
-# install jenkins
-
-#curl http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.8.5.tgz > mongo.tgz
+curl http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.8.5.tgz > /usr/local/src/mongo.tgz
+cd /usr/local/src
 tar -zxvvvf mongo.tgz
 
-mv mongodb-osx-x86_64-1.8.5/ /usr/local/var/mongo
+mv mongodb-osx-x86_64-1.8.5/ /usr/local/var/mongodb
 
-ln -sf /usr/local/var/mongo/bin/mongo /usr/local/bin
-ln -sf /usr/local/var/mongo/bin/mongod /usr/local/bin
+ln -sf /usr/local/var/mongodb/bin/mongo /usr/local/bin
+ln -sf /usr/local/var/mongodb/bin/mongod /usr/local/bin
 
+echo 'Go to your moody_friends directory, and run `mkdir -p tmp/db` then `mongod --db=tmp/db`'
