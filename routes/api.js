9304fd4700f658c2ade85138b6e006446bd34a77
Charles Bergeron <chuck.bergeron@gmail.com>
2013-02-09 19:52:26 -0700

    Breaking change: set up Twitter calls to use the user's access token and access token secret

---
+++ routes/api.js
@@ -0,0 +1,41 @@
+var _ = require('underscore');
+
+exports.index = function(request, response) {
+
+  var Twitter = new twit({
+    consumer_key: twitterConsumerKey,
+    consumer_secret: twitterConsumerSecret,
+    access_token: request.user.access_token,
+    access_token_secret: request.user.access_token_secret
+  });
+
+  var measureSentiment = function(text) {
+    var sentiment = SentimentEngine.classify(text);
+
+    if (sentiment.sentiment === "neutral") {
+      return 0;
+    }
+    if (sentiment.sentiment === "positive") {
+      return sentiment.probability;
+    }
+    return -1 * sentiment.probability;
+  };
+
+  Twitter.get('statuses/home_timeline', function(err, tweets) {
+    var feed = _.map(tweets, function(tweet) {
+      return {
+        twitter_id: tweet.user.id,
+        name: tweet.user.name,
+        screen_name: tweet.user.screen_name,
+        profile_url: tweet.user.profile_image_url,
+        source: 'twitter',
+        tweet_id: tweet.id,
+        text: tweet.text,
+        created_at: tweet.created_at,
+        sentiment: measureSentiment(tweet.text)
+      };
+    });
+
+    return response.send(feed);
+  });
+};
